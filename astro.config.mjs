de0ca37ad77e29895703cd3e4a1ed4a8cef3cedd
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2026-01-07 22:03:44 -0600

    favicon y seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://las-mananitas.vercel.app',
+  
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
